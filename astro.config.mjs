6853aa858a44b21bb752510c06693ebf45b6a6ff
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-11-07 11:04:32 +0000

    ajustes sitemap, actualizamos dependencias, añadimos meta datos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,23 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      customPages: [
+        "https://www.abitacolo.info/team/Juliette%20C.%20Mallen",
+        "https://www.abitacolo.info/team/Guillermo%20Perales",
+        "https://www.abitacolo.info/team/Manuel%20Ben",
+        "https://www.abitacolo.info/team/Alba%20Caldito%20G.",
+        "https://www.abitacolo.info/team/Maria%20Para%20R.",
+        "https://www.abitacolo.info/team/Cristina%20Allen%20Casal",
+        "https://www.abitacolo.info/team/Xavier%20Longo",
+        "https://www.abitacolo.info/team/Guillermo%20Correa",
+      ],
+    }),
+  ],
   output: "server",
-  site: "https://abitacolo-landing-p.vercel.app/",
+  site: "https://www.abitacolo.info/",
   adapter: vercel(),
 });
