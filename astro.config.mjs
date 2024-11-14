106878baa98bf9006e9f91f2cf3c03af4c44835c
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-11-14 12:36:11 +0000

    ajustes party/town

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,23 @@ import vercel from "@astrojs/vercel";
 import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap({
-    customPages: [
-      "https://www.abitacolo.info/team/Juliette%20C.%20Mallen",
-      "https://www.abitacolo.info/team/Guillermo%20Perales",
-      "https://www.abitacolo.info/team/Manuel%20Ben",
-      "https://www.abitacolo.info/team/Alba%20Caldito%20G.",
-      "https://www.abitacolo.info/team/Maria%20Para%20R.",
-      "https://www.abitacolo.info/team/Cristina%20Allen%20Casal",
-      "https://www.abitacolo.info/team/Xavier%20Longo",
-      "https://www.abitacolo.info/team/Guillermo%20Correa",
-    ],
-  }), partytown()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      customPages: [
+        "https://www.abitacolo.info/team/Juliette%20C.%20Mallen",
+        "https://www.abitacolo.info/team/Guillermo%20Perales",
+        "https://www.abitacolo.info/team/Manuel%20Ben",
+        "https://www.abitacolo.info/team/Alba%20Caldito%20G.",
+        "https://www.abitacolo.info/team/Maria%20Para%20R.",
+        "https://www.abitacolo.info/team/Cristina%20Allen%20Casal",
+        "https://www.abitacolo.info/team/Xavier%20Longo",
+        "https://www.abitacolo.info/team/Guillermo%20Correa",
+      ],
+    }),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
+  ],
   output: "server",
   site: "https://www.abitacolo.info/",
   adapter: vercel(),
